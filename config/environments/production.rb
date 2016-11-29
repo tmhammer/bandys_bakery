21eaf79d1864003995bd1a1ae3450c4e90845f1e
tmhammer <timh@govwizely.com>
2016-11-28 23:38:42 -0500

    Use gmail smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,16 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: "intense-basin-6406.herokuapp.com" }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'bandysbakery.com',
+  user_name:            'support@bandysbakery.com',
+  password:             ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
