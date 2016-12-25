8c74b9e382f4a182e4ebf27b7541753dc6171b78
tmhammer <timh@govwizely.com>
2016-12-24 19:23:57 -0500

    Check and modal for terms of services

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  before_filter :configure_permitted_parameters
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :terms)
+    end 
+  end
+
   private
 
   def after_inactive_sign_up_path_for(resource)
