94f003412fdab339a54e382f17184cfb5a48c6c6
tmhammer <tmhammer92@gmail.com>
2018-02-04 15:40:24 -0500

    Deprecation warnings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
-    :controllers => { :registrations => "registrations" }
+  #devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
+  #  :controllers => { :registrations => "registrations" }
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
